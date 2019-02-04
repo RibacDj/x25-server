93a88d1c1396c89cb7dcf57b001955424e1e99cc
joe <djordjeribac@gmail.com>
2019-02-04 13:58:23 +0100

    port change

--- server/app.js
+++ server/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 const userRoutes = require('./routes/user');
 const adminRoutes = require('./routes/admin');
 
 const app = express();
-
-app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', userRoutes);
 app.use('/admin', adminRoutes);
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,3 @@
-const path = require('path');
-
 exports.home = (req, res) => {
-  // es.send(path.join(global.appRoot, '/client/build/index.html'));
-  res.sendFile(path.join(global.appRoot, '/client/build/index.html'));
+  res.send('Home page');
 };
